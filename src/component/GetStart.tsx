145b027ea023d0dc64a6125ea4249d6eb9c3a6c0
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-08-18 15:49:55 +0300

    add component

---
+++ src/component/GetStart.tsx
@@ -0,0 +1,19 @@
+import styles from './GetStart.module.css';
+
+type Props = {
+  onStart: () => void;
+};
+
+const GetStart = ({ onStart }: Props) => {
+  return (
+    <section>
+      <div className={styles.bg}></div>
+      <div className={styles.greetings}>
+        <h1>Fall in Love with Coffee in Blissful Delight!</h1>
+        <p>Welcome to our cozy coffee corner, where every cup is a delightful for you.</p>
+        <span onClick={onStart} className={styles.btn}>Get Started</span>
+      </div>
+    </section>
+  );
+}
+export default GetStart;
