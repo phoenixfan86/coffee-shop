eb1676076a891b38f14d42150920d5f1545f8a4d
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-08-24 18:56:37 +0300

    change style

--- src/component/GetStart.tsx
+++ src/component/GetStart.tsx
@@ -7,7 +7,7 @@ type Props = {
 const GetStart = ({ onStart }: Props) => {
   return (
     <section className={styles.get_start}>
-      <div className={styles.bg}></div>
+      <div className={styles.bg}><img src="./img/bg.png" alt="" /></div>
       <div className={styles.greetings}>
         <h1>Fall in Love with Coffee in Blissful Delight!</h1>
         <p>Welcome to our cozy coffee corner, where every cup is a delightful for you.</p>
